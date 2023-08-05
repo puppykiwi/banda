2c428af279fe3ee14fd7c25a3af64454db375e8e
puppykiwi <johnraymwendwa04@gmail.com>
2023-08-05 11:38:24 +0300

    08

---
+++ 8.py
@@ -0,0 +1,5 @@
+fruits = ["apple", "banana", "orange", "apricot", "kiwi", "avocado", "pear", "grape", "mango", "lemon"]
+
+for fruit in fruits:
+    if fruit[0].lower() == "a":
+        print (fruit)
