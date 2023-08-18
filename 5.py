8d652eb80c046e6270931d25657f88067af3ded0
puppykiwi <johnraymwendwa04@gmail.com>
2023-08-18 13:50:41 +0000

    Deleted 5.py on Day 4

--- 5.py
+++
@@ -1,5 +0,0 @@
-fruits = ["apple", "banana", "orange", "apricot", "kiwi", "avocado", "pear", "grape", "mango", "lemon"]
-
-for fruit in fruits:
-    if fruit[0].lower() == "a":
-        print (fruit)
